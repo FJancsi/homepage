8b9074996cc7890816c6c84388d57932bc5bc8a4
FJancsi <fekete.janos2@gmail.com>
2020-03-23 09:34:21 +0100

    conditional footer info has been added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,21 @@
 <template>
-    <div class="footer">Memory: {{adaptiveData.memory}} GB | CPU: {{adaptiveData.cpu}} | Connection: {{adaptiveData.connection}} </div>
+    <div class="footer" v-if="adaptiveData">
+        <span v-if="adaptiveData.memory">Memory: {{adaptiveData.memory}} </span>
+        <span v-if="adaptiveData.cpu">CPU: {{adaptiveData.cpu}} </span>
+        <span v-if="adaptiveData.connection">Connection: {{adaptiveData.connection}}</span>
+    </div>
 </template>
 
 <script>
-import getAdaptiveLoadingData from 'adaptive-loading-helper';
+import getAdaptiveLoadingData from "adaptive-loading-helper";
 
 export default {
-    data: function () {
-        return {
-            adaptiveData: getAdaptiveLoadingData()
-        }
-    }
-}
+  data: function() {
+    return {
+        adaptiveData: getAdaptiveLoadingData()
+    };
+  }
+};
 </script>
 
 <style>
