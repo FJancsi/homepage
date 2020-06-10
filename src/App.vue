1a59bc1ff5d7fe94ac69923fa5491f4523e48590
FJancsi <fekete.janos2@gmail.com>
2020-06-10 17:03:29 +0200

    import db

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 import Firebase from "firebase";
+import db from "./services/db.js";
 
 export default {
   name: "app",
